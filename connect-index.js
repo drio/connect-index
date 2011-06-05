c0fa62cc61462721d2c40e65c0faba603e9252cd
David Rio Deiros <driodeiros@gmail.com>
2011-06-04 19:39:13 -0500

    * When dir not exists, pass the same request to next() middleware * Test for public dir in example

--- connect-index.js
+++ connect-index.js
@@ -1,30 +1,25 @@
-var fs     = require('fs'),
-    path   = require('path');
+var fs   = require('fs'),
+    path = require('path');
 
-/*
- * Our middleware (connect-autoindex)
- * Before the static middleware !!
- */
-module.exports = function(options) {
+module.exports = function(pub_dir) {
   return function(req, resp, next) {
-    var pub_dir      = __dirname + '/public';
     var filename     = path.join(pub_dir + req.url);
     var trimmed_path = filename.replace(pub_dir + "/", '');
-
-    path.exists(filename, function(exists) {  
+  
+    path.exists(filename, function(exists) {
       if (exists) { 
         fs.stat(filename, function (error, stats) {
           if (stats.isDirectory()) {
-            var body = 'listing here <br>' + '\n';
+            var body = 'listing here <hr>' + '\n';
             fs.readdir(filename, function(err, list) {
               if (err || !list) next(); // TODO
               list.forEach(function(f) {
-                var link = trimmed_path === '' 
+                var link = trimmed_path === ''
                            ? '/' + f 
                            : "/" + trimmed_path + "/" + f;
                 body += '<a href="' + link + '">' + f + '</a><br>' + "\n";
               });
-              resp.writeHead(200, {'Content-Type': 'text/html'}); 
+              resp.writeHead(200, {'Content-Type': 'text/html'});
               resp.end(body);
             });
           } else {
@@ -32,6 +27,9 @@ module.exports = function(options) {
           }
         }); 
       }
-    }); 
-  }  
+      else {
+        next();
+      }
+    });
+  }
 }
--- example.js
+++ example.js
@@ -1,19 +1,26 @@
 var connect = require('connect'),
-    connect_index = require('./connect-index');
+    connect_index = require('connect-index');
 
 var fs     = require('fs'),
     path   = require('path');
 
-var server = connect.createServer()
-  .use(connect.logger())
-  .use(connect_index())
-  .use(connect.static(__dirname + '/public', { maxAge: 0 }))
-  .use(function(req, res, next) {
-    var body = "default: " + req.url;
-    res.setHeader('Content-Length', body.length);
-    res.end(body);
-  });
+var pub_dir = __dirname + '/public';
+path.exists(pub_dir, function(exists) {
+  if (exists) {
+    var server = connect.createServer()
+      .use(connect.logger())
+      .use(connect_index(pub_dir))
+      .use(connect.static(pub_dir, { maxAge: 0 }))
+      .use(function(req, res, next) {
+        var body = "default: " + req.url;
+        res.setHeader('Content-Length', body.length);
+        res.end(body);
+      });
 
-server.listen(3000, "127.0.0.1");
-var a = server.address();
-console.log("Server started on: http://" + a.address + ":" + a.port);
+    server.listen(3000, "127.0.0.1");
+    var a = server.address();
+    console.log("Server started on: http://" + a.address + ":" + a.port);
+  } else {
+    console.log("Create your public dir please: " + pub_dir);
+  }
+});
